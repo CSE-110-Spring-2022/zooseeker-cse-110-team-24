7dad7d3b7ae7f36a62cc11a000f5cbabc22543a1
Yiran Wan <ewan951218@gmail.com>
2022-04-28 02:55:29 -0700

    searchBar with autocomplete; send search result to planner not yet

--- app/src/main/java/com/example/zooseekerteam24/SearchActivity.java
+++ app/src/main/java/com/example/zooseekerteam24/SearchActivity.java
@@ -5,24 +5,46 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.widget.AutoCompleteTextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
+import java.util.List;
+import java.util.Map;
+
 public class SearchActivity extends AppCompatActivity {
 
+    private static final String TAG = "SearchActivity";
     private BottomNavigationView btmNavi;
+    private AutoCompleteTextView searchBar;
+
+    private List<ZooData.Node> exhibits;
+
+//    private static final String[] COUNTRIES = new String[] {
+//            "Belgium", "France", "Italy", "Germany", "Spain", "FryRepublic"
+//    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
-
+        Log.d(TAG, "sanity");
         btmNavi = findViewById(R.id.btmNavi);
+        searchBar = findViewById(R.id.searchBar);
+
+        exhibits = ZooData.loadExhibitsFromJSON(this, "sample_exhibits.json");
+//        indexedExhibits.forEach((id, node) -> Log.d(TAG, node.toString()));
+
+
+        SearchResultAdapter adapter = new SearchResultAdapter(this, exhibits);
+//        ArrayAdapter<Node> adapter1 = new ArrayAdapter<>(this, android.R.layout.simple_dropdown_item_1line, indexedExhibits);
+        searchBar.setAdapter(adapter);
+
 
         btmNavi.setSelectedItemId(R.id.icSearch);
-
         btmNavi.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
