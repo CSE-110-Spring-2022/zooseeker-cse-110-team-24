2c3dea8f4581cf4e3629686c4c05e8b0cdf887ef
Wabadaba <58150904+Wabadaba@users.noreply.github.com>
2022-05-07 12:51:41 -0700

    Comments for RouteGenerator and DirectionActivity

--- app/src/main/java/com/example/zooseekerteam24/DirectionActivity.java
+++ app/src/main/java/com/example/zooseekerteam24/DirectionActivity.java
@@ -1,3 +1,10 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * File   : DirectionActivity.java
+ * Authors: Dylan Govic, Jose Valdivia, and Yiran Wan
+ * Desc   : Contains the necessary methods to handle the Direction Activity of
+ *          the ZooSeeker app.
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
 package com.example.zooseekerteam24;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,6 +24,7 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 public class DirectionActivity extends AppCompatActivity {
 
@@ -28,7 +36,6 @@ public class DirectionActivity extends AppCompatActivity {
 
     private RouteGenerator generator;
 
-    private List<ZooData.Node> targets;
     private Map<String,ZooData.Node> nodes;
     private Map<String,ZooData.Edge> edges;
     private Graph<String,IdentifiedWeightedEdge> g;
@@ -38,44 +45,48 @@ public class DirectionActivity extends AppCompatActivity {
 
     private List<Double> distanceList = new ArrayList<Double>();
     private int directionsIndex = -1;
+
     private List<Pair<String, Boolean>> directionsList;
-    //public TextView directionsText = (TextView) findViewById(R.id.directionsText);
 
-
+    /**
+     * Method: onCreate
+     * Desc  : Handles everything that the DirectionActivity must do on creation
+     * @param savedInstanceState the state used to check if any data is to be restored
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_direction);
 
-        // LOGIC FOR DIRECTIONS
-        // --------------->
-
+        // Converts each JSON data file into relevant data types
         nodes = ZooData.loadNodesFromJSON(this, nodeFile);
         edges = ZooData.loadEdgesFromJSON(this, edgeFile);
         g = ZooData.loadZooGraphJSON(this, graphFile);
 
+        // Creates empty routeGenerator object to be used to find the path
         generator = new RouteGenerator(this, exhibits, nodes, edges ,g );
 
+        // Sets the targets in the route generator to the exhibits that
+        // the user wishes to see
         NodeDao nodeDao = NodeDatabase.getSingleton(this).nodeDao();
         exhibits = nodeDao.getAll();
         generator.setTargets(exhibits);
+
+        // Generates a new path ONLY if a path doesn't already exist
         if(RouteGenerator.staticroute == null){
             route = generator.pathGenerator();
         } else {
             route = RouteGenerator.staticroute;
         }
 
+        // Generates the directions using the distances between each node in the route
         distanceList = generator.generateDistances(route);
-
         this.directionsList = generateDirections(exhibits,route,distanceList);
 
-        // <---------------
-
-
+        // Switches the activity the user is viewing if they click a button on the
+        // bottom navigation bar
         btmNavi = findViewById(R.id.btmNavi);
-
         btmNavi.setSelectedItemId(R.id.icDirection);
-
         btmNavi.setOnItemSelectedListener(item -> {
             switch (item.getItemId()){
                 case R.id.icSearch:
@@ -94,81 +105,105 @@ public class DirectionActivity extends AppCompatActivity {
         });
     }
 
+    /**
+     * Method: generateDirections
+     * Desc  : Generates a list of directions, and whether that direction leads the user
+     *         to one of the target exhibits that they wish to see
+     * @param targets      The list of exhibits that the user wishes to see
+     * @param route        The route that the user is set to take
+     * @param distanceList The list of distances between each exhibit on the route
+     * @return             A List of pairs of Strings that contain each direction, and
+     *                     Booleans that contain whether the given direction leads them
+     *                     to a target exhibit
+     */
     private List<Pair<String, Boolean>> generateDirections(List<ZooData.Node> targets,
                                                            List<ZooData.Node> route,
                                                            List<Double> distanceList) {
         StringBuilder sb = new StringBuilder();
         List<Pair<String, Boolean>> directionList = new ArrayList<Pair<String, Boolean>>();
+        // Performs a deep copy of targets so elements can be safely removed
         List<ZooData.Node> targCopy = generator.copyZooList(targets);
 
-        // used to check if the user has reached one of the target exhibits
+        // Used to check if the user has reached one of the target exhibits
         Boolean atExhibit;
 
+        // Iterates through each node in the route
         for (int i = 0; i < route.size()-1; i++) {
             atExhibit = false;
-            sb.append("("+(i+1)); // direction number
-            sb.append(")\n Walk ");
+            sb.append("("+(i+1)+")\n"); // direction number
+            sb.append("Walk ");
             sb.append(distanceList.get(i)); // distance to walk
             sb.append(" meters along\n");
-            // street name  |  this line will have to be thoroughly tested
-            sb.append(edges.get((g.getEdge(route.get(i).id,
-                                          route.get(i+1).id)).getId()).street);
-
+            sb.append(Objects.requireNonNull(edges.get((g.getEdge(route.get(i).id,
+                    route.get(i + 1).id)).getId())).street); // street name
             sb.append(" from\n");
             sb.append(route.get(i).name); // vertex 1 name
-            sb.append(" to\n");
+            sb.append("\nto");
             sb.append(route.get(i+1).name); // vertex 2 name
             sb.append(".");
 
-            //TODO figure out why targets is empty when calling the method
+            // Check if the direction leads you to a target exhibit
             for(int j = 0; j < targCopy.size(); j++){
+                // If true, set the atExhibit bool to true and remove from copy of targets
                 if (route.get(i + 1).id.equals(targCopy.get(j).id)) {
                     atExhibit = true;
                     targCopy.remove(j);
-                    //sb.append(" EXHIBIT");
                 }
             }
 
-            System.out.println(sb.toString());
+            // Add the new direction to the list
             directionList.add(new Pair<String, Boolean>(sb.toString(),atExhibit));
-
+            // Clear the stringbuilder for the next iteration
             sb.delete(0,sb.length());
         }
         return directionList;
     }
 
+    /**
+     * Method: onNextButtonClicked
+     * Desc  : Handles the clicking of the "Next" button
+     *         When clicked, iterates to the next direction in the list
+     * @param view   The button to be clicked
+     */
     public void onNextButtonClicked(View view) {
-
+        // Only iterate to the next direction if one exists
         if(directionsIndex < directionsList.size()-1){
             this.directionsIndex++;
+            // Updates the directions to the next on the list
             TextView directionsText = (TextView) findViewById(R.id.directionsText);
             directionsText.setText(directionsList.get(directionsIndex).first);
 
             TextView arrivedText = (TextView) findViewById(R.id.atExhibitText);
-            //if it is a target exhibit
+
+            // If it is a target exhibit
             if(directionsList.get(directionsIndex).second){
-                arrivedText.setText("ARRIVING AT EXHIBIT");
-            }
-            else{
+                arrivedText.setText("Arriving at Exhibit!");
+            } else {
                 arrivedText.setText("");
             }
         }
-
     }
 
+    /**
+     * Method: onPrevButtonClicked
+     * Desc  : Handles the clicking of the "Prev" button
+     *         When clicked, iterates to the previous direction in the list
+     * @param view   The button to be clicked
+     */
     public void onPrevButtonClicked(View view) {
-
+        //Only go to the previous button if you aren't at the start
         if(directionsIndex > 0){
             this.directionsIndex--;
+            // Updates the directions to the previous on the list
             TextView directionsText = (TextView) findViewById(R.id.directionsText);
             directionsText.setText(directionsList.get(directionsIndex).first);
 
             TextView arrivedText = (TextView) findViewById(R.id.atExhibitText);
-            //if it is a target exhibit
+
+            // If it is a target exhibit
             if(directionsList.get(directionsIndex).second){
                arrivedText.setText("ARRIVING AT EXHIBIT");
-            }
-            else{
+            } else {
                 arrivedText.setText("");
             }
         }
--- app/src/main/java/com/example/zooseekerteam24/RouteGenerator.java
+++ app/src/main/java/com/example/zooseekerteam24/RouteGenerator.java
@@ -1,23 +1,26 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * File   : RouteGenerator.java
+ * Authors: Dylan Govic and Jose Valdivia
+ * Desc   : Contains methods necessary in order to handle the Zoo route
+ *          Can generate the route from a list of target ZooData.Node's
+ *          and find the distances along the path
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
 package com.example.zooseekerteam24;
 
 import android.content.Context;
-import android.util.Log;
-import android.util.Pair;
 
 import androidx.annotation.NonNull;
 
 import org.jgrapht.Graph;
-import org.jgrapht.GraphPath;
 import org.jgrapht.alg.interfaces.ShortestPathAlgorithm;
 import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 public class RouteGenerator {
 
@@ -26,7 +29,8 @@ public class RouteGenerator {
     Map<String, ZooData.Node> nodes;
     Map<String, ZooData.Edge> edges;
     Graph<String, IdentifiedWeightedEdge> g;
-    static List<ZooData.Node> staticroute;
+
+    static List<ZooData.Node> staticroute; // Static so it can be refereced between activities
 
     public RouteGenerator(@NonNull Context context,
                           @NonNull List<ZooData.Node> targets,
@@ -40,11 +44,21 @@ public class RouteGenerator {
         this.g = g;
     }
 
-    // Performs a deep copy of targets
+    /**
+     * Method: setTargets
+     * Desc  : sets the targets instance variable to targets
+     * @param targets   targets to be set
+     */
     public void setTargets(List<ZooData.Node> targets){
         this.targets = copyZooList(targets);
     }
 
+    /**
+     * Method: copyZooList
+     * Desc  : performs a deep copy of a list of ZooData.Nodes so elements can be safely removed
+     * @param nodes   list of nodes to be copied
+     * @return        the copied list of nodes
+     */
     public List<ZooData.Node> copyZooList(List<ZooData.Node> nodes){
         List<ZooData.Node> newList = new ArrayList<ZooData.Node>();
         for(int i = 0; i < nodes.size(); i++) {
@@ -53,22 +67,40 @@ public class RouteGenerator {
         return newList;
     }
 
-    // TODO
-    public ZooData.Node getEntranceNode(){
-        return nodes.get("entrance_exit_gate");
-        //return null;
+    /**
+     * Method: getEntranceExitNode
+     * Desc  : Finds the ZooData.Node value of the entrance/exit gate of the Zoo by
+     *         iterating through the nodes and finding the kind "Gate"
+     * @return The Node of the entrance/exit gate
+     */
+    public ZooData.Node getEntranceExitNode() {
+        // Iterates through each entry of the list of nodes
+        Iterator<Map.Entry<String, ZooData.Node>> nodesIterator = nodes.entrySet().iterator();
+
+        while(nodesIterator.hasNext()) {
+            Map.Entry<String, ZooData.Node> mapElement = nodesIterator.next();
+            ZooData.Node.Kind kind = mapElement.getValue().kind;
+
+            // The kind Gate signifies entrance/exit, so return it if true
+            if(kind == (ZooData.Node.Kind.GATE)){
+                return mapElement.getValue();
+            }
+        }
+        System.err.println("No Entrance/Exit gate found.");
+        return null;
     }
 
-    // TODO
-    public ZooData.Node getExitNode(){
-        return nodes.get("entrance_exit_gate");
-        // return null;
-    }
-
-    // TODO
-
+    /**
+     * Method: nearestNode
+     * Desc  : Performs Dijkstra's algorithm to find the nearest target node from source
+     *         Returns the path through the zoo from path to the chosen node.
+     * @param source   Starting point node
+     * @return         A List of ZooData.Nodes that contains the shortest path through
+     *                 the zoo from source to target
+     */
     public List<ZooData.Node> nearestNode(ZooData.Node source)
     {
+        // Gives us all of the shortest paths from source to every node on the graph
         ShortestPathAlgorithm.SingleSourcePaths<String,IdentifiedWeightedEdge> ssPaths =
                 new DijkstraShortestPath<>(g).getPaths(source.id);
 
@@ -95,21 +127,32 @@ public class RouteGenerator {
         return returnPath;
     }
 
+    /**
+     * Method: pathGenerator
+     * Desc  : Generates an approximately optimal path between the target exhibits that the
+     *         user wants to visit. This is done by repeatedly finding the nearest unvisited
+     *         target node from where the user currently is using nearestNode
+     * @return A List of ZooData.Node's, in order from start to end, starting and ending at the
+     *         entrance/exit gate.
+     */
     public List<ZooData.Node> pathGenerator(){
-        // used to return the correct order of exhibits
+        // Used to return the correct order of exhibits
         List<ZooData.Node> route = new ArrayList<ZooData.Node>();
 
-        route.add(getEntranceNode());
+        // Route always starts at the entrance gate
+        route.add(getEntranceExitNode());
 
         ZooData.Node source;
         while(!(targets.isEmpty())) {
 
-            // set source to the last element in route
+            // Set source to the last element in route
             source = route.get(route.size()-1);
-            // removes last value of route to prevent duplicates
+
+            // Removes last value of route to prevent duplicates
             route.remove(route.size()-1);
             route.addAll(nearestNode(source));
-            // removes whatever target was added to the route from the targets list
+
+            // Removes whatever target was added to the route from the targets list
             for(int i = 0; i < targets.size(); i++){
                 if(targets.get(i).id.equals(route.get(route.size() - 1).id)){
                     targets.remove(i);
@@ -120,25 +163,32 @@ public class RouteGenerator {
         route.remove(route.size()-1);
 
         // find the path to the exit from the last exhibit
-        targets.add(getExitNode());;
+        targets.add(getEntranceExitNode());;
         route.addAll(nearestNode(source));
 
         staticroute = route;
         return route;
     }
 
-    // Generates Cumulative Distances ;)
+    /**
+     * Method: generateCumDistances
+     * Desc  : Generates the Cumulative distances along the path ;)
+     * @param route   The route used to generate the cumulative distances
+     * @return        List of Doubles containing the cumulative distances
+     */
     public List<Double> generateCumDistances(List<ZooData.Node> route){
         List<Double> returnList = new ArrayList<Double>();
 
+        // return if the route has less than 2 nodes, as that means it doesn't have edges
         if(route.size() < 2){
             return returnList;
         }
 
+        // Add the first edge, then iterate through remaining ones
         returnList.add((g.getEdge(route.get(0).id,route.get(1).id)).getWeight());
         for(int i = 1; i < route.size()-1; i++){
-            //holy FUCK
-
+            // Add the current edge weight plus the previous
+            // returnList entry to make it cumulative
             returnList.add((g.getEdge(route.get(i).id,
                     route.get(i+1).id)).getWeight() +
                     returnList.get(i-1));
@@ -146,9 +196,20 @@ public class RouteGenerator {
         return returnList;
     }
 
-    // Generates Individual Distances
+    /**
+     * Method: generateDistances
+     * Desc  : Generates the distances of each edge along the path
+     * @param route   The route used to generate each of the distances
+     * @return        List of Doubles containing the individual distances
+     */
     public List<Double> generateDistances(List<ZooData.Node> route){
         List<Double> returnDists = new ArrayList<Double>();
+
+        // return if the route has less than 2 nodes, as that means it doesn't have edges
+        if(route.size() < 2){
+            return returnDists;
+        }
+
         for(int i = 0; i < route.size()-1; i++){
             //holy FUCK
             returnDists.add((g.getEdge(route.get(i).id,
@@ -157,20 +218,28 @@ public class RouteGenerator {
         return returnDists;
     }
 
+    /**
+     * Method: exhibitDistances
+     * Desc  : Maps cumulative distances to each of the target exhibits
+     *         that the user wants to visit
+     * @param exhibits   The list of exhibits that the user wants to visit
+     * @return           Map of exhibit id's to their respective cumulative distance
+     */
     public Map<String, Double> exhibitDistances(List<ZooData.Node> exhibits){
         List<Double> distances;
         Map<String, Double> distanceMap = new HashMap<String, Double>();
-        List<ZooData.Node> exCopy = copyZooList(exhibits); //copied so we can remove safely
+        List<ZooData.Node> exCopy = copyZooList(exhibits); // Copied so we can remove safely
 
         staticroute = pathGenerator();
 
-        // generate cumulative distances
+        // Generate cumulative distances
         distances = generateCumDistances(staticroute);
 
-        // compare with the parameter
+        // Compare with the parameter
         for(int i = 0; i < staticroute.size()-1; i++) {
-            //iterate through all targets
+            // Iterate through all targets
             for(int j = 0; j < exCopy.size(); j++){
+                // If the current node on the route is an exhibit, add its distance
                 if(staticroute.get(i+1).id.equals(exCopy.get(j).id)){
                     distanceMap.put(staticroute.get(i + 1).id, distances.get(i));
                     exCopy.remove(j);
