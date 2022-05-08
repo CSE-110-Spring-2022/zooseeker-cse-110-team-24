8c874f9f1ce931710a6738bac4e5aee84eb1cd33
Rahul Puranam <rahulpuranam@Rahuls-MBP.lan>
2022-05-08 16:29:47 -0700

    add story tests for search and counter

---
+++ app/src/androidTest/java/com/example/zooseekerteam24/SearchValidMultiResult.java
@@ -0,0 +1,108 @@
+package com.example.zooseekerteam24;
+
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.replaceText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
+import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withParent;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.is;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class SearchValidMultiResult {
+
+    @Rule
+    public ActivityTestRule<SearchActivity> mActivityTestRule = new ActivityTestRule<>(SearchActivity.class);
+
+    @Test
+    public void searchValidMultiResult() {
+        ViewInteraction appCompatImageView = onView(
+                allOf(withClassName(is("androidx.appcompat.widget.AppCompatImageView")), withContentDescription("Search"),
+                        childAtPosition(
+                                allOf(withClassName(is("android.widget.LinearLayout")),
+                                        childAtPosition(
+                                                withId(R.id.searchView),
+                                                0)),
+                                1),
+                        isDisplayed()));
+        appCompatImageView.perform(click());
+
+        ViewInteraction searchAutoComplete = onView(
+                allOf(withClassName(is("android.widget.SearchView$SearchAutoComplete")),
+                        childAtPosition(
+                                allOf(withClassName(is("android.widget.LinearLayout")),
+                                        childAtPosition(
+                                                withClassName(is("android.widget.LinearLayout")),
+                                                1)),
+                                0),
+                        isDisplayed()));
+        searchAutoComplete.perform(replaceText("mammal"), closeSoftKeyboard());
+
+        ViewInteraction textView = onView(
+                allOf(withId(R.id.tvName), withText("Gorillas"),
+                        withParent(withParent(withId(R.id.lvResults))),
+                        isDisplayed()));
+        textView.check(matches(withText("Gorillas")));
+
+        ViewInteraction textView2 = onView(
+                allOf(withId(R.id.tvName), withText("Lions"),
+                        withParent(withParent(withId(R.id.lvResults))),
+                        isDisplayed()));
+        textView2.check(matches(withText("Lions")));
+
+        ViewInteraction textView3 = onView(
+                allOf(withId(R.id.tvName), withText("Elephant Odyssey"),
+                        withParent(withParent(withId(R.id.lvResults))),
+                        isDisplayed()));
+        textView3.check(matches(withText("Elephant Odyssey")));
+
+        ViewInteraction textView4 = onView(
+                allOf(withId(R.id.tvName), withText("Arctic Foxes"),
+                        withParent(withParent(withId(R.id.lvResults))),
+                        isDisplayed()));
+        textView4.check(matches(withText("Arctic Foxes")));
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
